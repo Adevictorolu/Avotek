473d2019d0b9852fcb9ce9c9a81598a907bd2687
Ademola Victor Oluokun <137506855+Adevictorolu@users.noreply.github.com>
2024-06-15 02:56:31 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\Flutter Sdk\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Programming\\avotek_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\Flutter Sdk\\flutter"
+  "PROJECT_DIR=F:\\Programming\\avotek_app"
+  "FLUTTER_ROOT=F:\\Flutter Sdk\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Programming\\avotek_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Programming\\avotek_app"
+  "FLUTTER_TARGET=F:\\Programming\\avotek_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lNzZjOTU2NDk4ODQxZTFhYjQ1ODU3N2QzODkyMDAzZTU1M2U0ZjNjLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Programming\\avotek_app\\.dart_tool\\package_config.json"
+)
